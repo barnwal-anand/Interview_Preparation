d4243b63f9e2eb303d62101f2c3ca22e913eed59
Anand Barnwal <anandtalks2u@gmail.com>
2021-09-15 18:41:41 +0530

    Create Minimum Window Substring.cpp

---
+++ Sliding Window Problem/Minimum Window Substring.cpp
@@ -0,0 +1,10 @@
+/*
+Leetcode: 76 - Minimum Window Substring
+Given two strings s and t of lengths m and n respectively,
+return the minimum window substring of s such that every character in t (including duplicates) is included in the window.
+If there is no such substring, return the empty string "".
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+*/
